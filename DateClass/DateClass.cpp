f2fd6a9bc2356d29fae0e6399698e324acb62f89
KarimaFathi <karimafathifadili@gmail.com>
2025-05-09 17:48:26 +0100

    add date library

---
+++ DateClass/DateClass.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "clsDate.h"
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+    clsDate date1(13,9,2000);
+
+    clsDate date2(300, 2025);
+    date2.Print();
+   /* date1.Print();*/
+   // cout << date1.isLeapYear();
+   
+ /*   cout << date1.NumberOfDaysFromTheBeginningOfTheYear();*/
+
+  /*  clsDate date2 = clsDate::GetDateFromDayOrderInYear(320, 2020);
+    date2.Print();*/
+
+
+   /* cout << date1.NumberOfDaysFromTheBeginningOfTheYear();*/
+
+    /*date1.addDaysToDate(2500);
+    date1.Print();*/
+
+ /*   cout << date1.IsDate1BeforeDate2(clsDate(11, 10, 2022));*/
+
+    //cout << clsDate::IsDate1BeforeDate2(clsDate(10, 10, 2022), clsDate(11, 10, 2022));
+
+    //cout << date1.isLeapYear();
+    //cout << date1.areDatesEqual(clsDate(10, 10, 2022));
+
+   /* date1.increaseDateByOneDay();
+    date1.Print();*/
+
+    /*cout << date1.isLeapYear();*/
+
+    /*cout << date1.calculateDifferenceInDays(clsDate(10, 10, 2024));*/
+
+    //cout << clsDate::CalculateMyAgeInDays(clsDate(13,9,2000));
+
+
+
+  /*  date1.decreaseDateByOneDecade();
+    date1.Print();*/
+
+    //cout << date1.determineDaysUntilTheEndOfYear();
+    
+}
